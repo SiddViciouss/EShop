a2656496a6cebc2680e0f25ed0f09aae11e3d35f
Anatoly Ivanov <anatoly.ivanov@girngm.ru>
2019-01-13 13:29:28 +0500

    SQLite packages added, publish profile for linux

--- EShop.DataModel/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EShop.DataModel
-{
-    public class Class1
-    {
-    }
-}
---
+++ EShop.DataModel/Context/ShopContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.DataModel.Context
+{
+    //public class ShopContext: DbContext
+    //{
+    //    //public DbSet<>  { get; set; }
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    {
+    //        optionsBuilder.UseSqlite("Data Source=eshop.db");
+    //    }
+    //}
+}
