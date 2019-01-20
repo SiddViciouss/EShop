b5d3a163d3ecc943615432d7a26890d6893db859
Anatoly Ivanov <anatoly.ivanov@girngm.ru>
2019-01-20 14:38:21 +0500

    Intermediate commit

---
+++ EShop.Web/Code/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using EShop.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Web.Code
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+
+        }
+    }
+}
