c46a9862b99cd336058beb77bcfa040fa432612b
Anatoly Ivanov <anatoly.ivanov@girngm.ru>
2019-01-21 02:59:48 +0500

    Dynamic product and category listing

--- EShop.Web/Code/DbInitializer.cs
+++ EShop.Web/Code/DbInitializer.cs
@@ -55,8 +55,8 @@ namespace EShop.Web.Code
                     Description = "Длина - 27мм(полная)\\n Ширина - 27мм(широчайшая часть)\\n Толщина - 5мм(самое толстое место) Абаш термический, ювелирная смола, фурнитура сталь. Финиш натуральной мастикой из льняного масла и пчелиного воска.",
                     Price = 600,
                     Tag = "Кольца",
-                    PreviewImagePath = "~/upload/1.jpg",
-                    ImagePaths = "~/upload/1.jpg",
+                    PreviewImagePath = "/upload/1.jpg",
+                    ImagePaths = "/upload/1.jpg",
                     AddedDate = DateTime.Now,
                     ModifiedDate = DateTime.Now
                 },
@@ -69,23 +69,151 @@ namespace EShop.Web.Code
                     Description = "Размер 18. Дерево кедр, ювелирная смола.Финиш натуральной мастикой из льняного масла и пчелиного воска.",
                     Price = 1200,
                     Tag = "Кольца",
-                    PreviewImagePath = "~/upload/2.jpg",
-                    ImagePaths = "~/upload/2.jpg",
+                    PreviewImagePath = "/upload/2.jpg",
+                    ImagePaths = "/upload/2.jpg",
                     AddedDate = DateTime.Now,
                     ModifiedDate = DateTime.Now
-                }//,
-                //new Product()
-                //{
-                //    Id = 1,
-                //    CategoryId = 0,
-                //    Code = "K0001",
-                //    Name = "",
-                //    Description = "",
-                //    Price = 100,
-                //    Tag = "",
-                //    AddedDate = DateTime.Now,
-                //    ModifiedDate = DateTime.Now
-                //}
+                },
+                new Product()
+                {
+                    Id = 3,
+                    CategoryId = 1,
+                    Code = "K0003",
+                    Name = "Кольцо \"Chipped lava\"",
+                    Description = "Размер 17,5. Дерево дуб шпон в 4 слоя, ювелирная смола, Люминесцент - бирюзовый(светится в темноте). Финиш натуральной мастикой из льняного масла и пчелиного воска.",
+                    Price = 1600,
+                    Tag = "Кольца",
+                    PreviewImagePath = "/upload/3.jpg",
+                    ImagePaths = "/upload/3.jpg",
+                    AddedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now
+                },
+                new Product()
+                {
+                    Id = 4,
+                    CategoryId = 1,
+                    Code = "K0004",
+                    Name = "Кольцо \"Ice block in the blizzards\"",
+                    Description = "Размер 17. Абаш термический, ювелирная смола, необработанное стекло с подкраской, Люминесцент - синий(светится в темноте).Финиш натуральной мастикой из льняного масла и пчелиного воска.",
+                    Price = 2500,
+                    Tag = "Кольца",
+                    PreviewImagePath = "/upload/4.jpg",
+                    ImagePaths = "/upload/4.jpg",
+                    AddedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now
+                },
+                new Product()
+                {
+                    Id = 5,
+                    CategoryId = 2,
+                    Code = "С0001",
+                    Name = "Серьги \"Little violet glow\"",
+                    Description = "Длина - 61мм(полная) Ширина - 8мм(лицевая часть) Абаш термический, ювелирная смола, фурнитура сталь, Люминесцент-зелёный(светится в темноте). Финиш натуральной мастикой из льняного масла и пчелиного воска.",
+                    Price = 1800,
+                    Tag = "Серьги",
+                    PreviewImagePath = "/upload/5.jpg",
+                    ImagePaths = "/upload/5.jpg",
+                    AddedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now
+                },
+                new Product()
+                {
+                    Id = 6,
+                    CategoryId = 1,
+                    Code = "K0005",
+                    Name = "Кольцо \"Icy gloom\"",
+                    Description = "Размер 16. Абаш термический, ювелирная смола. Финиш натуральной мастикой из льняного масла и пчелиного воска.",
+                    Price = 1550,
+                    Tag = "Кольца",
+                    PreviewImagePath = "/upload/6.jpg",
+                    ImagePaths = "/upload/6.jpg",
+                    AddedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now
+                },
+                new Product()
+                {
+                    Id = 7,
+                    CategoryId = 2,
+                    Code = "С0002",
+                    Name = "Серьги \"Half balance\"",
+                    Description = "Длина - 38мм(полная) Ширина - 6мм(лицевая часть) Абаш светлый, абаш термический, ювелирная смола, фурнитура сталь, Люминесцент-зелёный(светится в темноте). Финиш морилкой и натуральной мастикой из льняного масла и пчелиного воска.",
+                    Price = 1300,
+                    Tag = "Серьги",
+                    PreviewImagePath = "/upload/7.jpg",
+                    ImagePaths = "/upload/7.jpg",
+                    AddedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now
+                },
+                new Product()
+                {
+                    Id = 8,
+                    CategoryId = 4,
+                    Code = "Б0001",
+                    Name = "Брошь \"Strata guitar\"",
+                    Description = "Длина - 82мм(полная) Ширина - 31мм(широкая часть деки) Толщина - 8мм(дека) Абаш светлый, ювелирная смола, частично пигмент с блестками, фурнитура сталь. Финиш натуральной мастикой из льняного масла и пчелиного воска.",
+                    Price = 1300,
+                    Tag = "Броши",
+                    PreviewImagePath = "/upload/8.jpg",
+                    ImagePaths = "/upload/8.jpg",
+                    AddedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now
+                },
+                new Product()
+                {
+                    Id = 9,
+                    CategoryId = 1,
+                    Code = "K0006",
+                    Name = "Кольцо \"Incorporeal\"",
+                    Description = "Размер 17,5. Дерево кедра, ювелирная смола. Финиш морилкой, натуральной мастикой из льняного масла и пчелиного воска.",
+                    Price = 950,
+                    Tag = "Кольца",
+                    PreviewImagePath = "/upload/9.jpg",
+                    ImagePaths = "/upload/9.jpg",
+                    AddedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now
+                },
+                new Product()
+                {
+                    Id = 10,
+                    CategoryId = 1,
+                    Code = "K0007",
+                    Name = "Кольцо \"Acrylic riot\"",
+                    Description = "Размер 17. Дерево дуб, ювелирная смола, Включения из белого и черного акрила. Финиш натуральной мастикой из льняного масла и пчелиного воска.",
+                    Price = 1500,
+                    Tag = "Кольца",
+                    PreviewImagePath = "/upload/10.jpg",
+                    ImagePaths = "/upload/10.jpg",
+                    AddedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now
+                },
+                new Product()
+                {
+                    Id = 11,
+                    CategoryId = 1,
+                    Code = "K0008",
+                    Name = "Кольцо \"Pond bottom\"",
+                    Description = "Размер 20. Дерево ели, ювелирная смола, акрил, пигмент окраски с блёстками. Финиш морилкой, натуральной мастикой из льняного масла и пчелиного воска.",
+                    Price = 1200,
+                    Tag = "Кольца",
+                    PreviewImagePath = "/upload/11.jpg",
+                    ImagePaths = "/upload/11.jpg",
+                    AddedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now
+                },
+                new Product()
+                {
+                    Id = 12,
+                    CategoryId = 1,
+                    Code = "K0009",
+                    Name = "Кольцо \"Light sections\"",
+                    Description = "Размер 17. Дерево пальмы, ювелирная смола. Финиш натуральной мастикой из льняного масла и пчелиного воска.",
+                    Price = 800,
+                    Tag = "Кольца",
+                    PreviewImagePath = "/upload/12.jpg",
+                    ImagePaths = "/upload/12.jpg",
+                    AddedDate = DateTime.Now,
+                    ModifiedDate = DateTime.Now
+                },
             };
             try
             {
